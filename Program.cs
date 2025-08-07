9e95868ebf66e1afa511dfc4e527fdc8c3513a35
deepakushwaha17 <deepakushwaha155@gmail.com>
2025-08-07 11:37:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAssignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Q1 q1 = new Q1();
+            q1.Sum();
+        }
+    }
+}
---
+++ Q1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAssignment1
+{
+     class Q1
+    {
+        public void Sum()
+        {
+            int sum = 0;
+            int[] a = { 200, -150, 340, 500, -100 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum = sum + a[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
